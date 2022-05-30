4158b7dacfe89fe723e9488984b55d078f555e8f
Fed-14 <federica.citarrella14@gmail.com>
2022-05-30 12:46:08 +0200

    Update

--- transaction/src/main/java/com/bankIsland/transaction/service/TransactionService.java
+++
@@ -1,9 +0,0 @@
-package com.bankIsland.transaction.service;
-
-import com.bankIsland.transaction.entity.Transaction;
-
-public interface TransactionService {
-
-    public Transaction save(Transaction transaction);
-
-}
