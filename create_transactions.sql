6f6a9428d1405e21c518482cb92c6af284bd0431
Fed-14 <federica.citarrella14@gmail.com>
2022-05-30 12:47:00 +0200

    Update

---
+++ create_transactions.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS `transactions`;
+
+CREATE DATABASE  IF NOT EXISTS `transactions`;
+USE `transactions`;
+
+DROP TABLE IF EXISTS `transactions`;
+CREATE TABLE `transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` tinyint NOT NULL,
+  `account_number_from` varchar(45) NOT NULL,
+  `account_number_to` varchar(45) DEFAULT NULL,
+  `account_owner_id` int(11) NOT NULL,
+  `date` datetime NOT NULL,
+  `cause` varchar(100) DEFAULT NULL,
+  `amount` double NOT NULL,
+  PRIMARY KEY (`id`)
+--   CONSTRAINT `account_owner_id_ibfk_1` FOREIGN KEY (`account_owner_id`) REFERENCES `account_owners`.`account_owners` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+SET FOREIGN_KEY_CHECKS = 1;
