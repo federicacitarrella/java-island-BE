b172d5bdc669c100a2b5f7e361f84897a0f44662
Fed-14 <federica.citarrella14@gmail.com>
2022-06-09 17:19:48 +0200

    D

--- transaction/src/main/java/com/bankIsland/transaction/rabbit/message/TransactionMessage.java
+++
@@ -1,5 +0,0 @@
-package com.bankIsland.transaction.rabbit.message;
-
-public record TransactionMessage (String accountNumber,
-                                  double amount,
-                                  int accountOwnerId) {}
