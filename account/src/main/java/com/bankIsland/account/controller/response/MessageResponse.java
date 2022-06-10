1cbdb3f0dc839230afc0f8e02ab38f4b22b9c5a7
Fed-14 <federica.citarrella14@gmail.com>
2022-06-10 16:04:20 +0200

    D

--- account/src/main/java/com/bankIsland/account/controller/response/MessageResponse.java
+++
@@ -1,3 +0,0 @@
-package com.bankIsland.account.controller.response;
-
-public record MessageResponse (String message) {}
--- transaction/src/main/java/com/bankIsland/transaction/controller/request/TransactionRequest.java
+++
@@ -1,7 +0,0 @@
-package com.bankIsland.transaction.controller.request;
-
-public record TransactionRequest (String accountNumber,
-                                  int type,
-                                  double amount,
-                                  String cause) {}
-
