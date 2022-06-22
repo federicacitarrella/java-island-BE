d4185edc5e40a7444ec0d88cd8d0d670e7d931b2
Fed-14 <federica.citarrella14@gmail.com>
2022-06-22 15:42:31 +0200

    D

--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-docker run -d -p 5672:5672 -p 15672:15672 --name my-rabbit rabbitmq:3-management
-start cmd /k "cd %1\eureka-server && mvnw spring-boot:run -e"
-start cmd /k "cd %1\api-gateway && mvnw spring-boot:run -e"
-start cmd /k "cd %1\user && mvnw spring-boot:run -e"
-start cmd /k "cd %1\transaction && mvnw spring-boot:run -e"
-start cmd /k "cd %1\account && mvnw spring-boot:run -e"
-
